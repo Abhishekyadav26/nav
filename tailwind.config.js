d6216fe941d2e52ecea9b3d186b4e8d1a8799ed1
Abhishekyadav26 <aky732244@gmail.com>
2024-07-08 20:23:00 +0530

    files

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {},
+    fontfamily: {
+      'display' : ['poppins','sans-serif'],
+      'body': ['inter', 'sans-serif']
+    }
+  },
+  plugins: [],
+}
+
