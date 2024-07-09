223c87ca28d82c6c77ad1156fe05a1c12aee3b23
Abhishekyadav26 <aky732244@gmail.com>
2024-07-10 00:41:03 +0530

    updated main section

--- script.js
+++ script.js
@@ -2,3 +2,40 @@ const navDialog = document.getElementById('nav-dialog')
 function handlemenu (){
     navDialog.classList.toggle('hidden');
 }
+
+function setupIntersectionObserver (element, isLTR, speed){
+    const intersectioncallback = (entries) => {
+        const isIntersecting = entries[0].isIntersecting;
+        if(isIntersecting){
+            document.addEventListener('scroll', scrollhandler);
+        }
+        else{
+            document.removeEventListener('scroll', scrollhandler);
+        }
+    }
+    const intersectioncallobserver = new IntersectionObserver(intersectioncallback)
+    
+    IntersectionObserver.observal(element);
+    
+    function scrollHandler() {
+        const translateX = (window.innerHeight - element.getBoundingClientRect().top) * speed;
+
+        let totalTranslate = 0;
+        if(isLTR) {
+            totalTranslate = translateX + initialTranslateLTR;
+        } else {
+            totalTranslate = -(translateX + initialTranslateRTL);
+        }
+
+        element.style.transform = `translateX(${totalTranslate}px)`;
+    }
+
+}
+
+const line1 = document.getElementById('line1');
+const line2 = document.getElementById('line2');
+const line3 = document.getElementById('line3');
+
+setupIntersectionObserver(line1, true,0.15);
+setupIntersectionObserver(line2, true,0.15);
+setupIntersectionObserver(line3, true,0.15);
